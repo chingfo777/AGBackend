6382ac36424f4d59e008f586b14a8764f5556bf9
chingfo777 <103221221+chingfo777@users.noreply.github.com>
2024-05-20 22:42:39 +0530

    #8

--- User/models.py
+++ User/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+import uuid
 
 class User(models.Model):
-    user_id = models.AutoField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=50)
     address=models.CharField(max_length=200)
